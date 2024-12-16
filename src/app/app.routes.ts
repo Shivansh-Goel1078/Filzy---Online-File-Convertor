6c96cae89b25440e1b5b34399d3dff4fd764c9f1
Shivansh Goel <shivansh.goela12@gmail.com>
2024-12-16 21:26:17 +0530

    Init Project

---
+++ src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { FileUploadComponent } from './Components/file-upload/file-upload.component';
+
+export const routes: Routes = [
+    {
+        path:'File-upload',
+        component: FileUploadComponent,
+        title: "File Upload | Filzy"
+    }
+];
